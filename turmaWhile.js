88773c1d1c77024f94e02bf374822b0455372777
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-10-21 23:53:51 -0300

    Create turmaWhile.js

---
+++ turmaWhile.js
@@ -0,0 +1,11 @@
+let turma = [
+  { nome: "João", nota: 7 },
+  { nome: "Maria", nota: 9 },
+  { nome: "Pedro", nota: 6 }
+];
+
+let i = 0;
+while (i < turma.length) {
+  console.log(turma[i].nome + ": " + turma[i].nota);
+  i++;
+}
