44a2ac02ee69e72e11c1d74ac01aef4f42f86fd9
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-10-22 00:08:31 -0300

    Update casosWhile.js

--- casosWhile.js
+++ casosWhile.js
@@ -9,6 +9,7 @@ while (i <= 5) {
   console.log(i);
   i++;
 }
+console.log(); // Imprime uma linha em branco
 
 // ☑ Etapas lógicas envolvidas:
 // Lógica: repetir 5 vezes, começando em 1 e aumentando até 5. 
@@ -22,6 +23,7 @@ while (isNaN(numero) || numero < 1 || numero > 10) {
   numero = parseInt(prompt("Valor inválido. Digite um número entre 1 e 10:"));
 }
 console.log("Número válido:", numero);
+console.log(); // Imprime uma linha em branco
 // Lógica: Enquanto o número digitado for inválido (NaN) ou estiver fora da faixa de 1 a 10, o programa deve solicitar novo valor.
 
 //☑ Exemplo 3: Solicitar senha até que seja correta
@@ -30,6 +32,7 @@ while (senha !== "1234") {
   senha = prompt("Digite a senha:");
 }
 console.log("Acesso liberado!");
+console.log(); // Imprime uma linha em branco
 // Lógica: Enquanto a senha digitada for diferente de "1234", o programa deve continuar solicitando uma nova senha.
 
 // ☑ Exemplo 4: Somar valores digitados até ultrapassar 100
@@ -41,15 +44,17 @@ while (soma <= 100) {
   }
 }
 console.log("Soma final:", soma);
+console.log(); // Imprime uma linha em branco
 // Lógica: Enquanto a Lógica for menor ou igual a 100, o programa continua solicitando valores e acumulando o total.
 
 // ☑ Exemplo 5: Gerar números aleatórios até sair um múltiplo de 7 (execute mais vezes para ver geração variada de números).
-let numero = 0;
-while (numero % 7 !== 0 || numero === 0) {
-  numero = Math.floor(Math.random() * 100);
-  console.log("Gerado:", numero);
+let number = 0;
+while (number % 7 !== 0 || number === 0) {
+  number = Math.floor(Math.random() * 100);
+  console.log("Gerado:", number);
 }
-console.log("Múltiplo de 7 encontrado:", numero);
+console.log("Múltiplo de 7 encontrado:", number);
+console.log(); // Imprime uma linha em branco
 // Lógica: Enquanto o número gerado não for múltiplo de 7 ou igual a 0, o programa continua gerando valores aleatórios.
 
 // ☑ Exemplo 6: Solicitar palavra até que comece com “A”
