7d13a37c388fdff74d8428ad6c8fc29e8c2729b2
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2026-03-29 13:02:12 -0300

    Add teste2103.js with variable definitions and console logs

---
+++ teste2103.js
@@ -0,0 +1,13 @@
+// Definição das variáveis
+let nome = 'Gabriel Mendonça';
+let idade = 25;
+
+// Impressão no console 
+console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade}`);
+
+/* IMPORTANTE! 
+Para interpolar (=incluir) variáveis em strings no JS, 
+utilize template literals com crases (``). 
+A sintaxe correta é `${variável}` dentro da string, 
+por exemplo: ``console.log(`Nome: ${nome}`)``. */
